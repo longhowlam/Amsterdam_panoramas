8536a09054ec82dec25167afecc69625aad1735a
longhowlam <longhowlam@gmail.com>
2018-04-23 08:37:01 +0100

    shiny app

--- App.R
+++ App.R
@@ -9,25 +9,31 @@ ui <- dashboardPage(
     sidebarMenu(
       menuItem("Inleiding", tabName = "introduction", icon = icon("dashboard")),
       menuItem("Beelden classificaties", tabName = "imagestab", icon = icon("th")),
-      selectInput("imagelabel", "image label", clf_classes2$entities)
+      selectInput("imagelabel", "image label", clf_classes2$entities),
+      checkboxInput("randombeelden", "toon ook de random beelden")
     )
   ),
   dashboardBody(
     tabItems(
       tabItem(
         tabName = "introduction",
-        h4("Introduction"),
+        h4("Inleiding"),
         list(
-            p("De gemeente Amsterdam heeft panoramabeelden van Amsterdam beschikbaar gesteld."),
-            a(href="https://api.data.amsterdam.nl/panorama/", "zie hier"),
+            p(
+              "De gemeente Amsterdam heeft panoramabeelden van Amsterdam beschikbaar gesteld.",
+              a(href="https://api.data.amsterdam.nl/panorama/", "Zie hier."),
+              "Een camera auto heeft door Amsterdam gereden en foto's genomen. Ik heb per buurt in Amsterdam 400 random images gepakt
+              en deze door ",
+             a(href = "https://www.clarifai.com/models/general-image-recognition-model-aaa03c23b3724a16a56b629203edc62c", "Clarifai image model"),
+             "heen gehaald. Per image krijg je dan een aantal labels."
+             ),
             
-            p("Een camera auto heeft door Amsterdam gereden en foto's genomen. Ik heb per buurt in Amsterdam 400 random images gepakt
-              en deze door het clarifai image model heen gehaald. Per image krijg je dan een aantal labels"),
             p(" "),
-            p("Wordcloud image labels van Amsterdam"),
-            img(SRC="amsterdamlabels.png", height = 300),
+            p("Onderstaande wordcloud geeft een overzicht van de labels die het clatifai model teruggeeft op de panoramabeelden van Amsterdam"),
+            img(SRC="amsterdamlabels.png", height = 250),
             p(" "),
-            p("Deze shiny app laat het percentage per buurt zien van een gekozen label"),
+            p("Deze shiny app laat het percentage per buurt zien van een gekozen label, klick op de buurt voor animated gifs en klik op een punt voor
+              een specifiek image."),
             p("Cheers, Longhow")
                 )
               
@@ -52,7 +58,7 @@ server <- function(input, output) {
 
   output$images = renderLeaflet({
     
-    #### create the shapes
+    #### create the data for shapes for the map
     Amsterdam_image_perc = Amsterdam_Clarifai_classes %>% 
       mutate(
         imlabel = entities == input$imagelabel 
@@ -76,33 +82,64 @@ server <- function(input, output) {
       "' width='325' height='200'>"
     )
     
-    #### create set for markers
-    locs = Amsterdam_Clarifai_classes2 %>% 
-      filter(
-        entities == input$imagelabel 
-      )
-    polpopup2 = paste0(
-      "<img border='0' src='",
-      locs$link,
-      "' width='325' height='200'>"
-    )
+    labels <- sprintf(
+      "<strong>%s</strong><br/>%g procent dichtheid van label %s",
+      AmsterdamWijken3$BU_NAAM, round(100*AmsterdamWijken3$imlabel,2), input$imagelabel
+    ) %>% lapply(htmltools::HTML)
+    
+    
     ### maak leaflet
     m = leaflet()
     m = m %>%
-      setView(lng = 4.899431, lat = 52.379189, zoom = 12) %>%
+      setView(lng = 4.959431, lat = 52.34, zoom = 12) %>%
       addTiles() %>%
       addPolygons(
         weight=2,
         smoothFactor = 0.2, fillOpacity = 0.55, 
         color = ~colorQuantile(n = 7, "Greens", AmsterdamWijken3$imlabel)(imlabel),
         data = AmsterdamWijken3,
-        popup = polpopup )  %>% 
-      addCircleMarkers(
-        lng=~x, lat = ~y, radius = 0.5,
-        opacity = .21,
-        popup = polpopup2,
-        data = locs
+        popup = polpopup ,
+        highlight = highlightOptions(
+          weight = 5,
+          color = "#666",
+          dashArray = "",
+          fillOpacity = 0.7,
+          bringToFront = TRUE),
+        label = labels,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto")
       )
+    
+    if(input$randombeelden){
+      
+    #### create set for markers for the random selected images
+      locs = Amsterdam_Clarifai_classes2[.(input$imagelabel )]
+      polpopup2 = paste0(
+        "<strong>panorambeeld geclassificeerd als ", 
+        input$imagelabel, "</strong> <br/>",
+        "<img border='0' src='",
+        locs$link,
+        "' width='315' height='200'>"
+      )
+      m  = leaflet() %>%
+        setView(lng = 4.959431, lat = 52.34, zoom = 13) %>%
+        addTiles() %>%
+        addPolygons(
+          weight=2,
+          smoothFactor = 0.2, fillOpacity = 0.55, 
+          color = ~colorQuantile(n = 6, "Greens", AmsterdamWijken3$imlabel)(imlabel),
+          data = AmsterdamWijken3 
+        ) %>% 
+          addCircleMarkers(
+            lng=~x, lat = ~y, radius = 0.4,
+            opacity = .421,
+            popup = polpopup2,
+            data = locs
+          )
+      }
+   
     m
     
   })
